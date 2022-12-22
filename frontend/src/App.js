a580108a6e40a31a92578242486223b5f7899098
Your Name <karkibishal00@gmail.com>
2022-12-22 17:52:30 +1100

    ui, add and convert task done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,72 @@
 import "./App.css";
+import { Container } from "react-bootstrap";
+import { AddTaskForm } from "./components/AddTaskForm";
+import { ListArea } from "./components/ListArea";
+import { useState } from "react";
+
+const weeklyHour = 7 * 24;
 
 function App() {
-  return <>xfcghjk</>;
+  // state to store tasklist
+  const [tasksList, setTasksList] = useState([]);
+
+  // total hours of all tasklist - reduce
+  const totalHours = tasksList.reduce((acc, item) => acc + +item.hours, 0);
+
+  // function to recieve tasklist from search form and update tasklist
+  const addTask = (task) => {
+    // excedded hours
+    if (totalHours + +task.hours > weeklyHour) {
+      alert("You have excedded your weekly hours");
+      return;
+    }
+
+    setTasksList([...tasksList, task]);
+  };
+
+  // console.log(tasksList);
+  // After we receive the taskList we then use them in list area by passing as props
+
+  // switch function to transfer task from entry to bad
+  const switchTask = (id, type) => {
+    // console.log(i, type);
+
+    const switchArr = tasksList.map((item, index) => {
+      if (item.id === id) {
+        item.type = type;
+        // return (item.type = type);
+        // const dt = (item.type = type);
+        // return dt;
+
+        // return { ...item, type: type };
+      }
+      return item;
+    });
+
+    // logic of switch from entry to bad is
+    // if index of task is equal to i then change the type to bad
+    // else return the item
+
+    setTasksList(switchArr);
+  };
+
+  return (
+    <div className="wrapper">
+      <Container>
+        <h1 className="text-center py-5">My Not ToDo List</h1>
+        {/* form */}
+        <AddTaskForm addTask={addTask} />
+
+        <hr />
+        {/* list */}
+        <ListArea
+          tasksList={tasksList}
+          switchTask={switchTask}
+          totalHours={totalHours}
+        />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/TaskList.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Button, Form, Table } from "react-bootstrap";
+
+export const TaskList = ({ title, arrow, list = [], switchTask }) => {
+  //   console.log(list);
+  // after receiving list, we have to map the list and display it in table
+  return (
+    <div className="mt-3">
+      <h2 className="text-center">{title}</h2>
+      <div className="table">
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>
+                {" "}
+                <Form.Check type="checkbox" label="" />
+              </th>
+              <th>Task</th>
+              <th>Hours</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((item, i) => (
+              <tr key={i}>
+                <td>
+                  {" "}
+                  <Form.Check type="checkbox" label="" />
+                </td>
+                <td>{item.task}</td>
+                <td>{item.hours} hours</td>
+                <td>
+                  {arrow === "right" ? (
+                    <Button
+                      variant="success"
+                      onClick={() => switchTask(item.id, "bad")}
+                    >
+                      <i className="fas fa-arrow-right"></i>
+                    </Button>
+                  ) : (
+                    <Button
+                      variant="danger"
+                      onClick={() => switchTask(item.id, "entry")}
+                    >
+                      <i className="fas fa-arrow-left"></i>
+                    </Button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  );
+};
